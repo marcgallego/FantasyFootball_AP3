de6238c53bd8636ea38227b78a83af65068bc97b
Marc Gàllego <mga2525@gmail.com>
2019-12-12 17:55:46 +0100

    Provando

--- run_all.sh
+++ run_all.sh
@@ -5,3 +5,10 @@ for i in `seq 1 9`; do
     ./a.out benchs/data_base$i.txt benchs/query$i.txt benchs/solution$i.txt
     echo "_______________________________________________________________"
 done
+
+for i in `seq 1 9`; do
+    echo "\nJoc de proves $i"
+    ./checker.linux64 benchs/data_base$i.txt benchs/query$i.txt benchs/solution$i.txt
+    echo "_______________________________________________________________"
+done
+
