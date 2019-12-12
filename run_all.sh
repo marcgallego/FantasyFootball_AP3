8e0c5ab78261d7c2c813f82036a8384882ae577e
Marc Gàllego <mga2525@gmail.com>
2019-12-12 16:47:58 +0100

    Donat un arxiu a.out, executa el programa per a tots els jocs de prova

---
+++ run_all.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+for i in `seq 1 9`; do
+    echo "\nJoc de proves $i"
+    ./a.out benchs/data_base$i.txt benchs/query$i.txt benchs/solution$i.txt
+    echo "_______________________________________________________________"
+done
