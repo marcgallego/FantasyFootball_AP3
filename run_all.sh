fe992fddb9d3068f3e78f07cbb7de320be51de96
Marc Gàllego <mga2525@gmail.com>
2019-12-13 09:20:58 +0100

    run_all adaptat a mac

--- run_all.sh
+++ run_all.sh
@@ -3,6 +3,6 @@
 for i in `seq 1 9`; do
     echo "\nJoc de proves $i"
     ./a.out benchs/data_base$i.txt benchs/query$i.txt benchs/solution$i.txt
-    ./checker.linux64 benchs/data_base$i.txt benchs/query$i.txt benchs/solution$i.txt
+    ./checker.MacOS benchs/data_base$i.txt benchs/query$i.txt benchs/solution$i.txt
     echo "_______________________________________________________________"
 done
